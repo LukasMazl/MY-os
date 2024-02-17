09e9d697ce9b8edb6002946829a3516f872d1c1e
lukas.mazl <lukas.mazl@tul.cz>
2024-02-17 11:28:08 +0100

    added tts

--- src/kernel.c
+++ src/kernel.c
@@ -9,6 +9,7 @@
 #include "disk/disk.h"
 #include "fs/pparser.h"
 #include "fs/file.h"
+#include "task/tss.h"
 #include "gdt/gdt.h"
 #include "config.h"
 #include "memory/memory.h"
@@ -19,12 +20,17 @@ void panic(const char* message)
     while(1) {}
 }
 
+struct tss tss;
+
 struct gdt gdt_real[MYOS_TOTAL_GDT_SEGMENTS];
 struct gdt_structured gdt_structured[MYOS_TOTAL_GDT_SEGMENTS] = 
 {
     {.base = 0x00, .limit = 0x00, .type = 0x00}, // NULL segment
     {.base = 0x00, .limit = 0xffffffff, .type = 0x9a}, // Kernel code segment
-    {.base = 0x00, .limit = 0xffffffff, .type = 0x92} // Kernel data segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0x92}, // Kernel data segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf8}, // User code segment
+    {.base = 0x00, .limit = 0xffffffff, .type = 0xf2},  // User data segment
+    {.base = (uint32_t)&tss, .limit=sizeof(tss), .type= 0xE9} //TSS segment
 };
 
 static struct paging_4gb_chunk* kernel_chunk = 0;
@@ -50,6 +56,14 @@ void kernel_main()
     
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // Setup TSS
+    memset(&tss, 0x00, sizeof(tss));
+    tss.esp0 = 0x600000;
+    tss.ss0 = KERNEL_DATA_SELECTOR;
+
+    // Load tss
+    tss_load(0x28);
 
     // Setup paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
